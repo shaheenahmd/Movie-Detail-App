fdab5c62aebd20e5040643dd3336351838cf324e
shaheenahmd <shaheenahmdk@gmail.com>
2023-08-04 15:51:12 +0530

    builded

--- src/components/movieDetail/Movie.jsx
+++ src/components/movieDetail/Movie.jsx
@@ -4,7 +4,7 @@ import './Movie.css'
 
 const Movie = () => {
 
-const[currentMovieDetails,setMovie]= useState();
+const[currentMovieDetail,setMovie]= useState();
 const {id} = useParams();
 
 useEffect(()=>{
@@ -20,22 +20,49 @@ const getData = ()=> {
 }
 
 
-  return (
-
-    <div className='movie'>
-
-      <h1>{currentMovieDetails? currentMovieDetails.original_title :''}</h1>
-
-      <div className="image">
-        <img src={`https://image.tmdb.org/t/p/original${currentMovieDetails? currentMovieDetails.backdrop_path : ""}`} alt="" className='img-fluid'/>
+return (
+  <div className="movie">
+      <div className="movie__intro">
+          <img className="movie__backdrop" src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.backdrop_path : ""}`} />
       </div>
-      
-      <div className="">
-        <p>{currentMovieDetails? currentMovieDetails.original_title :''}</p>
-      </div>
-    </div>
 
-  )
+      <div className="movie__detail">
+          <div className="movie__detailLeft">
+              <div className="movie__posterBox">
+                  <img className="movie__poster" src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.poster_path : ""}`} />
+              </div>
+          </div>
+          <div className="movie__detailRight">
+              <div className="movie__detailRightTop">
+                  <div className="movie__name">{currentMovieDetail ? currentMovieDetail.original_title : ""}</div>
+                  <div className="movie__tagline">{currentMovieDetail ? currentMovieDetail.tagline : ""}</div>
+                  <div className="movie__rating">
+                      {currentMovieDetail ? currentMovieDetail.vote_average: ""} <i class="fas fa-star" />
+                      <span className="movie__voteCount">{currentMovieDetail ? "(" + currentMovieDetail.vote_count + ") votes" : ""}</span>
+                  </div>  
+                  <div className="movie__runtime">{currentMovieDetail ? currentMovieDetail.runtime + " mins" : ""}</div>
+                  <div className="movie__releaseDate">{currentMovieDetail ? "Release date: " + currentMovieDetail.release_date : ""}</div>
+                  <div className="movie__genres">
+                      {
+                          currentMovieDetail && currentMovieDetail.genres
+                          ? 
+                          currentMovieDetail.genres.map(genre => (
+                              <><span className="movie__genre" id={genre.id}>{genre.name}</span></>
+                          )) 
+                          : 
+                          ""
+                      }
+                  </div>
+              </div>
+              <div className="movie__detailRightBottom">
+                  <div className="synopsisText"></div>
+                  <div>{currentMovieDetail ? currentMovieDetail.overview : ""}</div>
+              </div>
+              
+          </div>
+      </div>
+  </div>
+)
 }
 
 export default Movie
